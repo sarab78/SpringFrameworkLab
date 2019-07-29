236e6cd929de8c047cb1f90355ecd2874e5b9c9e
sarab78 <sarbat_kaur@hotmail.co.uk>
2019-07-29 14:43:24 +0100

    PirateService_LabInSpring

---
+++ pirateservice/src/test/java/com/codeclan/example/pirateservice/PirateserviceApplicationTests.java
@@ -0,0 +1,28 @@
+package com.codeclan.example.pirateservice;
+
+import com.codeclan.example.pirateservice.models.Pirate;
+import com.codeclan.example.pirateservice.repositories.PirateRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PirateserviceApplicationTests {
+
+	@Autowired
+	PirateRepository pirateRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void createPirate() {
+		Pirate jack = new Pirate("Jack", "Sparrow", 32);
+		pirateRepository.save(jack);
+
+	}
+}
